7f854351e17ebf68a657170965734537a299cdc9
publizm <publizm@gmail.com>
2019-12-17 16:53:10 +0900

    docs: custom hook refactoring with Reducer

--- playground/src/useInputs.js
+++ playground/src/useInputs.js
@@ -1,14 +1,40 @@
-import { useState, useCallback } from 'react';
+import { useReducer, useCallback } from 'react';
+
+function reducer(form, action) {
+  switch (action.type) {
+    case 'CHANGE_FORM':
+      return {
+        ...form,
+        [action.name]: action.value,
+      };
+    case 'RESET_FORM':
+      return Object.keys(form).reduce((acc, current) => {
+        console.log(acc);
+        acc[current] = '';
+        return acc;
+      }, {});
+    default:
+      throw new Error('Unhandled action')
+  }
+}
 
 function useInputs(initialForm) {
-  const [form, setForm] = useState(initialForm);
+  const [form, dispatch] = useReducer(reducer, initialForm);
 
   const onChange = useCallback(e => {
     const { name, value } = e.target;
-    setForm(form => ({ ...form, [name]: value }))
+    console.log(name, value)
+    dispatch({
+      type: 'CHANGE_FORM',
+      name,
+      value
+    })
   }, []);
 
-  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  const reset = useCallback(() => {
+    console.log('리셋')
+    dispatch({type: 'RESET_FORM'})
+  }, []);
 
   return [form, onChange, reset];
 };

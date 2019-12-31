45355cb07cde8d91e3ecc1084b2e19a72c128022
publizm <publizm@gmail.com>
2019-12-31 16:52:16 +0900

    docs: study Custom petch

--- react-hooks/src/CustomInput.js
+++ react-hooks/src/CustomInput.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function useInput(defaultValue) {
   const [value, setValue] = useState(defaultValue);
@@ -10,8 +11,32 @@ function useInput(defaultValue) {
   return { value, onChange };
 }
 
+function useFetch(url) {
+  const [payload, setPayload] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const callUrl = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setPayload(data);
+    } catch (err) {
+      setError('error');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    callUrl();
+  }, []);
+
+  return { payload, loading, error };
+}
+
 export default function CustomInput() {
   const name = useInput('');
+  const { payload, loading, error } = useFetch('http://aws.random.cat/meow');
   console.log(name);
   return (
     <div>
@@ -19,7 +44,9 @@ export default function CustomInput() {
       {/* 동일 */}
       {/* <input value={name.value} onChange={name.onChange} type="text" /> */}
       <input {...name} type="text" />
-      {name.value}
+      {loading && <span>loading your cat</span>}
+      {!loading && error && <span>{error}</span>}
+      {!loading && payload && <img src={payload.file} alt="ㅎㅎㅎ" />}
     </div>
   );
 }

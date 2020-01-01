a57a0ec94c9bdac31d41a02d5c01c5e22d2f95ee
publizm <publizm@gmail.com>
2020-01-01 16:28:01 +0900

    docs: Practice making useClick Hook with useEffect Hook

---
+++ react-hooks/src/customHooks/CustomClick.js
@@ -0,0 +1,23 @@
+import React, { useRef, useEffect } from 'react';
+
+export const useClick = onClick => {
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      if (typeof onClick !== 'function') {
+        return;
+      }
+
+      element.current.addEventListener('click', onClick);
+    }
+
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener('click', onClick);
+      }
+    };
+  }, []);
+
+  return element;
+};
--- react-hooks/src/customHooks/index.jsx
+++ react-hooks/src/customHooks/index.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import uuid from 'uuid';
 import { useInput } from './CustomInput';
 import { useFetch } from './CustomFetch';
 import { useTabs } from './CustomTabs';
 import { useTitle } from './CustomTitle';
+import { useClick } from './CustomClick';
 
 export default function Custom() {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("home"), 1000);
-
+  const sayHello = () => {
+    console.log('say HEllo')
+  }
+  const title = useClick(sayHello);
   return (
     <>
-      <div>
-      </div>
+      <h1 ref={title}>Hi!</h1>
     </>
   );
 }

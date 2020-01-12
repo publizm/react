5c2c02843efeed5ea3f6cdb0276ea688ce87e351
publizm <publizm@gmail.com>
2020-01-12 19:45:23 +0900

    docs: study hooks with game

--- game/src/App.js
+++ game/src/App.js
@@ -1,39 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const App = () => {
-  const [myPick, setMyPick] = useState('');
-  const [computerPick, setComputerPick] = useState('');
+  const [mySelect, setMySelect] = useState('');
+  const [computerSelect, setComputerSelect] = useState('');
   const [whoWins, setWins] = useState('');
+  const select = useRef();
 
-  const runGame = value => {
+  const runGame = () => {
+    const value = select.current.value;
     const items = ['가위', '바위', '보'];
     const randomIdx = Math.floor(Math.random() * items.length);
-    setMyPick(value);
-    setComputerPick(items[randomIdx]);
+    setMySelect(value);
+    setComputerSelect(items[randomIdx]);
   };
 
   useEffect(() => {
-    if (myPick === '가위') {
-      if (computerPick === '가위') setWins('무승부');
-      if (computerPick === '바위') setWins('컴퓨터 승리');
-      if (computerPick === '보') setWins('유저 승리');
-    } else if (myPick === '바위') {
-      if (computerPick === '가위') setWins('유저 승리');
-      if (computerPick === '바위') setWins('무승부');
-      if (computerPick === '보') setWins('컴퓨터 승리');
-    } else if (myPick === '보') {
-      if (computerPick === '가위') setWins('컴퓨터 승리');
-      if (computerPick === '바위') setWins('유저 승리');
-      if (computerPick === '보') setWins('무승부');
+    if (mySelect === '가위') {
+      if (computerSelect === '가위') setWins('무승부');
+      if (computerSelect === '바위') setWins('컴퓨터 승리');
+      if (computerSelect === '보') setWins('유저 승리');
+    } else if (mySelect === '바위') {
+      if (computerSelect === '가위') setWins('유저 승리');
+      if (computerSelect === '바위') setWins('무승부');
+      if (computerSelect === '보') setWins('컴퓨터 승리');
+    } else if (mySelect === '보') {
+      if (computerSelect === '가위') setWins('컴퓨터 승리');
+      if (computerSelect === '바위') setWins('유저 승리');
+      if (computerSelect === '보') setWins('무승부');
     }
-  }, [myPick, computerPick]);
+  }, [mySelect, computerSelect]);
 
   return (
     <div>
       {whoWins}
-      <button onClick={() => runGame('가위')}>가위</button>
-      <button onClick={() => runGame('바위')}>바위</button>
-      <button onClick={() => runGame('보')}>보</button>
+      <select ref={select} onChange={runGame}>
+        <option value="가위">가위</option>
+        <option value="바위">바위</option>
+        <option value="보">보</option>
+      </select>
     </div>
   );
 };
